a1c7ce02df2c74e01d74b0ab6c4a2b6df1fcb153
Paul <pgekeler@gmail.com>
2026-07-09 15:40:31 +0200

    chore: Expose functions to public API and bump version.

--- jaxtree_extensions/__init__.py
+++ jaxtree_extensions/__init__.py
@@ -1,3 +1,22 @@
 """jaxtree_extensions - Extensions of JAX pytree utilities."""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
+
+from jaxtree_extensions.compare import TreeDiff, tree_compare
+from jaxtree_extensions.enumerate import tree_map_enumerate
+from jaxtree_extensions.path_select import tree_at_path, tree_select_by_path
+from jaxtree_extensions.prune import PrunedTreeDef, tree_map_prune
+from jaxtree_extensions.reduce_path import tree_reduce_with_path
+from jaxtree_extensions.zip_mismatched import tree_zip_mismatched
+
+__all__ = [
+    "tree_map_prune",
+    "PrunedTreeDef",
+    "tree_select_by_path",
+    "tree_at_path",
+    "tree_zip_mismatched",
+    "tree_map_enumerate",
+    "tree_compare",
+    "TreeDiff",
+    "tree_reduce_with_path",
+]
